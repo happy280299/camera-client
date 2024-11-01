803531a713e99cec61a5cdca3d34210f09bf42f8
Thịnh Trần <thinhtv@uniorbit.org>
2024-11-01 14:36:24 +0700

    test

--- client.js
+++ client.js
@@ -1,40 +1,11 @@
-const WebSocket = require("ws");
-const NodeWebcam = require("node-webcam");
+const ffmpeg = require('ffmpeg-stream');
+const http = require('http');
 
-// Configure webcam to return buffer directly
-const webcamOpts = {
-  width: 1280,
-  height: 720,
-  quality: 100,
-  delay: 0,
-  saveShots: false,
-  output: "jpeg",
-  callbackReturn: "buffer", // Ensures data is returned as buffer
-  verbose: false,
-  device: "/dev/video0"
-};
+http.createServer((req, res) => {
+  const args = ['-f', 'v4l2', '-i', '/dev/video0', '-f', 'mpeg1video', '-b:v', '800k', '-r', '30', '-'];
+  const ffmpegProcess = ffmpeg.spawn(args);
 
-const webcam = NodeWebcam.create(webcamOpts);
-
-// Connect to Render server WebSocket URL
-const serverUrl = "https://camera-nodejs.onrender.com"; // Replace with Render server URL
-const ws = new WebSocket(serverUrl);
-
-ws.on("open", () => {
-  console.log("Connected to the Render server");
-
-  // Capture and send image every second
-  setInterval(() => {
-    webcam.capture((err, data) => { // No filename needed, as it returns buffer
-      if (err) {
-        console.error("Error capturing image:", err);
-        return;
-      }
-      ws.send(data.toString("base64")); // Send image data in base64 format
-    });
-  }, 1000);
-});
-
-ws.on("error", (err) => {
-  console.error("WebSocket error:", err);
+  ffmpegProcess.stdout.pipe(res);
+}).listen(8080, () => {
+  console.log('Streaming video on http://192.168.122.24:8080');
 });
